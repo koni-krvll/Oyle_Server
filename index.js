cc3af768eb2e88a20da60440ee4c4e5a3815b11f
Gerard Marquina Rubio <gerardmarquinarubio@gmail.com>
2022-08-22 09:23:00 +0200

    Changes

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ app.use(Express.json());
 
 app.use('/api/v1', require('./routes'));
 
+require('./models/client');
+
 const listener = app.listen(process.env.PORT || 3000, () => {
     console.log('Your app is listening on ' + JSON.stringify(listener.address()));
 });
--- routes/user.js
+++ routes/user.js
@@ -7,16 +7,6 @@ const { register, login } = require('../controllers/user');
 const check = require('../middlewares/check');
 const auth = require('../middlewares/auth');
 
-// TODO: Add checkers
-router.post('/create', check(register, {
-    body: [
-        { name: 'email', checker: () => true },
-        { name: 'phone', checker: () => true },
-        { name: 'password', checker: () => true },
-        { name: 'name', checker: () => true },
-    ],
-}));
-
 // TODO: Add default checker
 router.get('/login', auth, login);
 
